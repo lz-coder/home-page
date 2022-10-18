c3f78b49ef46ecb7955b85d9327aabeb26b4672a
luan-adao <luanadao98@gmail.com>
2022-10-18 00:11:49 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch ("./engines.json")
+    .then((res) => res.json())
+    .then((data) => {
+        let searchEngine = document.querySelectorAll(".search-engine");
+        for (i=0, i<=searchEngine.length,i++) {
+            
+        }
+    });
